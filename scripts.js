70e02cdd26a3817faee1e36d0cb5bd2cf4f9312e
TonyDeleo98 <Tonyd9819@gmail.com>
2023-07-04 12:20:52 -0400

    scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,11 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
+    element.addEventListener("click", myFunction);
+    
+  });
+
+  // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
     // function? How can DOM traversal be used to get the "hour-x" id of the
@@ -20,4 +24,3 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
-  });
